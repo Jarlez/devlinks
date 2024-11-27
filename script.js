2a2b0071c1a34a7c7605ff7e9575250260673b61
Jarles <62815538+Jarlez@users.noreply.github.com>
2024-11-27 09:10:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // pegar a tag img
   const img = document.querySelector("#profile img")
   const logo = document.querySelector(".logo-js")
-
-  // substituir a imagem
+  
   if (html.classList.contains("light")) {
-    // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
     logo.setAttribute("src", "./assets/JSlogoBlack.png")
   } else {
-    // set tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
     logo.setAttribute("src", "./assets/JSlogo.png")
   }
